2359b018a4eab9862a3dfefaadbac932e569b534
Zach Lambert <zachlambert@github.com>
2023-09-30 11:17:44 +0100

    Minor fix

--- include/cpp_utils/darray.h
+++ include/cpp_utils/darray.h
@@ -12,7 +12,7 @@ template <typename T, int Dim, typename Allocator = std::allocator<T>>
 using darray = std::conditional_t<Dim != -1, std::array<T, Dim>, std::vector<T, Allocator>>;
 
 template <typename T, int Dim>
-void resize_darray(core::darray<T, Dim>& darray, std::size_t size) {
+void resize_darray(darray<T, Dim>& darray, std::size_t size) {
     if constexpr (Dim != -1) {
         assert(size == darray.size());
     }
